08df7969074561898ee12ee624931c7cd6b8ef0f
Jack Somers <somersjw@umich.edu>
2019-08-05 16:19:56 +0100

    adding routes

---
+++ html/index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'objects/library/Router/Request.php';
+require_once 'objects/library/Router/Router.php';
+
+use objects\library\Router\Request;
+use objects\library\Router\Router;
+
+$request = new Request();
+$router = new Router($request);
+
+$router->get('/', function () {
+	//include 'include.html';
+	include 'index.html';
+});
+
+$router->get('/teacher', function() {
+    include 'teacher.html';
+});
