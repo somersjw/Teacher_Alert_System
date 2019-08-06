4ed5fa36d02d95f11cf8edc634187d3e8ff435d2
Jack Somers <somersjw@umich.edu>
2019-08-06 21:01:01 +0100

    yeet

--- html/backend/dataAccess/AlertSystemDbGateway.php
+++ html/backend/dataAccess/AlertSystemDbGateway.php
@@ -6,7 +6,7 @@ class AlertSystemDbGateway {
     private $db;
 
     public function __construct() {
-        $this->db = new mysqli("localhost", "test", "test", "alertsystem");
+        $this->db = mysqli_connect("localhost", "test", "test", "alertsystem");
     }
 
     public function getSites() {
