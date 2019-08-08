2453c6f9d737b25785c197658fca10e5e9bfcd56
Jack Somers <somersjw@umich.edu>
2019-08-08 19:27:59 +0100

    yeet

--- html/index.php
+++ html/index.php
@@ -88,7 +88,9 @@ $router->post('/resetDemo', function() {
 	header('Location: /teacher');
 });
 
-$router->delete('/api/alert-system/alert/{alertId}', function($alertId) {
+$router->delete('/api/alert-system/alert', function() {
 	$apiController = new AlertSystemApiController();
+	$postdata = file_get_contents("php://input");
+	$request = json_decode($postdata, true);
 	// return json_encode($apiController->deleteAlert((int)$alertId));
 });
